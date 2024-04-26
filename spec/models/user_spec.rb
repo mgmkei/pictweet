ce057452cafbc9837eef863162a1e4f197c90655
mgmkei <rstoyo3.mizugami.keisuke@techcamp-training.com>
2024-04-26 13:44:51 +0900

    エクスペクテーションを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     it 'nicknameが空では登録できない' do
       user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
       user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
       # emailが空では登録できないテストコードを記述します
