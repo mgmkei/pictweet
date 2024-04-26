af417c2571af824e30c92ef4a5a0287ab7f45db2
mgmkei <rstoyo3.mizugami.keisuke@techcamp-training.com>
2024-04-26 13:30:39 +0900

    生成したインスタンスに対してバリデーションを実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
       user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user.valid?
     end
     it 'emailが空では登録できない' do
       # emailが空では登録できないテストコードを記述します
