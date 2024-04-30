229199653acf81abf2f119a1f124ec45972f730e
mgmkei <rstoyo3.mizugami.keisuke@techcamp-training.com>
2024-04-30 15:31:30 +0900

    新規登録がうまくいかないときのテストコードを修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -36,26 +36,32 @@ RSpec.describe 'ユーザー新規登録', type: :system do
 
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
-      it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
-        # トップページに移動する
-        visit root_path
-        # トップページにサインアップページへ遷移するボタンがあることを確認する
-        expect(page).to have_content('新規登録')
-        # 新規登録ページへ移動する
-        visit new_user_registration_path
-        # ユーザー情報を入力する
-        fill_in 'Nickname', with: ''
-        fill_in 'Email', with: ''
-        fill_in 'Password', with: ''
-        fill_in 'Password confirmation', with: ''
-        # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
-        expect{
-          find('input[name="commit"]').click
-          sleep 1
-        }.to change { User.count }.by(0)
-        # 新規登録ページへ戻されることを確認する
-        expect(page).to have_current_path(new_user_registration_path)
-      end
+      # トップページに移動する
+<<<<<<< HEAD
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'Nickname', with: ''
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
+      fill_in 'Password confirmation', with: ''
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(page).to have_current_path(new_user_registration_path)
+=======
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      # 新規登録ページへ移動する
+      # ユーザー情報を入力する
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      # 新規登録ページへ戻されることを確認する
+>>>>>>> parent of 564cf54 (新規登録がうまくいかないときのテストコードを記述)
     end
   end
 end
