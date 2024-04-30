147c857f2363d4ad8457da1d66dba8ef2019e540
mgmkei <rstoyo3.mizugami.keisuke@techcamp-training.com>
2024-04-30 13:38:28 +0900

    indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在するのテストコードを編集

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -11,6 +11,8 @@ describe TweetsController, type: :request do
       expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+      get root_path
+      expect(response.body).to include(@tweet.text)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
     end
